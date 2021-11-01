6efec16a790b422cf8ef879bbb190c2236a2f8a1
AnnDribnokhod <dribnohod93@gmail.com>
2021-11-01 03:42:22 +0300

    3010

--- KarmiSion/js/script.js
+++ KarmiSion/js/script.js
@@ -6,4 +6,45 @@ window.addEventListener('DOMContentLoaded', () => {
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('nav__right_links-active');
-    });})
+    });});
+
+/*
+    $(document).ready(function(){
+        $('.situation__items_item_block-plus').click(function(){
+            $('.situation__items_item_content').slideToggle(300, function(){
+                if ($(this).is(':hidden')) {
+                    $('.situation__items_item_block-plus').removeClass('open');
+                } else {
+                    $('.situation__items_item_block-plus').addClass('open');
+                }							
+            });
+            return false;
+        });
+    });
+
+    $(document).ready(function(){
+        $('.situation__items_item_block-plus').click(function(){
+            $('.situation__items_item_content').slideToggle(300, function(){
+                if ($(this).is(':hidden')) {
+                    $(this).removeClass('open');
+                } else {
+                    $(this).addClass('open');
+                }							
+            });
+            return false;
+        });
+    });
+/*
+$(document).ready(function(){
+    $('.situation__items_item_block-plus').click(function(){
+     $( this ).children(".situation__items_item_content" ).toggle(500);
+    });
+  });
+
+
+  $(document).ready(function() {
+	$('.situation__items_item_block').on('click', '.situation__items_item_block-plus', function() {
+		$(this).toggleClass('.open').siblings('.situation__items_item_content').slideToggle(80);
+	});
+
+  });*/
